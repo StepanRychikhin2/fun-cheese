d11caa5169315573e3b270cc61ec529eccc8b6f0
qqwwpro <111581882+StepanRychikhin2@users.noreply.github.com>
2024-08-28 10:01:14 +0300

    save

--- src/components/pages/LoadPage/InfoPanel.jsx
+++ src/components/pages/LoadPage/InfoPanel.jsx
@@ -1,13 +1,36 @@
-import sty from "./loadPagecss.module.css"
+import sty from './loadPagecss.module.css'
 import { React, Component } from 'react'
+import img from '../../../img/LoadPageimg/LoadingPageFoodImg.png'
+import imgYT from '../../../img/LoadPageimg/youTubeStart.png'
+
 class InfoPanelLoadPage extends Component {
 	render() {
 		return (
-			<div >
-				<p>awdawd</p>
+			<div className={sty.boxInfoPanel}>
+				<div>
+					<div className={sty.boxTextFood}>
+						<h1 className={sty.TitleFod}>Fast</h1>
+						<p className={sty.TxtFod}>Food Delivery</p>
+					</div>
+
+					<p className={sty.TxtInfo}>
+						Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium
+						doloremque laudantium
+					</p>
+					<div>
+						<button className={sty.btnOrder}>Order Now</button>
+						<button className={sty.btnVideo}>
+							<img className={sty.imgYT} src={imgYT} /><p>Watch Video</p> 
+						</button>
+					</div>
+				</div>
+
+				<div>
+					<img className={sty.imgLoadINfo} src={img} />
+				</div>
 			</div>
 		)
 	}
 }
 
-export default InfoPanelLoadPage;
+export default InfoPanelLoadPage
